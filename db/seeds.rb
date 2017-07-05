368d76cbc8acf13618e7390ad4d548fb16d1d36d
Sandoval Danilo <sandovaldanilo.lm@gmail.com>
2017-07-05 01:58:04 -0300

    Add some creatures to seeds.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Creature.create(name: 'Personagem 1', dexterity: 10, alert_feat: false)
+Creature.create(name: 'Personagem 2', dexterity: 12, alert_feat: false)
+Creature.create(name: 'Personagem 3', dexterity: 14, alert_feat: false)
+Creature.create(name: 'Personagem 4', dexterity: 12, alert_feat: true)
